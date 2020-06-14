e5e22bfff330b3d26db3512677e9c374ab6ac487
Agatha Rudko <agatharudko@gmail.com>
2020-06-14 21:48:30 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,23 @@ module.exports = {
 
 
   entry: './src/index.js',
-
-
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+             ],
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
